726686d596d32f1d27d0f4a7ede0ddacfefb79de
azim mia <141178400+Azim-Mia@users.noreply.github.com>
2024-01-31 10:35:30 +0600

    Create routing.js
    add file

---
+++ routing.js
@@ -0,0 +1,36 @@
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    useParams,
+} from "react-router-dom";
+ 
+function BlogPost() {
+    let { id } = useParams();
+    return (
+        <div style={{ fontSize: "50px" }}>
+            Now showing post {id}
+        </div>
+    );
+}
+ 
+function Home() {
+    return <h3>home page </h3>;
+}
+ 
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/page/:id">
+                    <BlogPost />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+ 
+export default App;
